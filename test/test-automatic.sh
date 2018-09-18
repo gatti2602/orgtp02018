168f8ba8f59184f6afc0ba3ac097208db678023b
Lucas Veron <veron_lucas@hotmail.com>
2018-09-18 01:13:42 -0300

    cambios en test

--- test/test-automatic.sh
+++ test/test-automatic.sh
@@ -126,6 +126,7 @@ echo "#---------# COMIENZA test ejercicio encode/decode random #--------#"
 
 n=1;
 while :; do
+#while [$n -lt 10]; do
 head -c $n </dev/urandom >/tmp/in.bin;
 ./tp0 -a encode -i /tmp/in.bin -o /tmp/out.b64;
 ./tp0 -a decode -i /tmp/out.b64 -o /tmp/out.bin;
@@ -133,8 +134,8 @@ if diff /tmp/in.bin /tmp/out.bin; then :; else
 echo ERROR: $n;
 break;
 fi
-echo ok: $n;
-n="‘expr $n + 1‘";
+echo [OK]: $n;
+n=`expr $n + 1`;
 rm -f /tmp/in.bin /tmp/out.b64 /tmp/out.bin
 done
 
